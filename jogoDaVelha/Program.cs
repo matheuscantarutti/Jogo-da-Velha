73b5c10e6269698e5ab08f0d620a12eef99c1894
Matheus Cantarutti <47987856+matheuscantarutti@users.noreply.github.com>
2020-06-25 18:24:41 -0300

    Jogadores e ordem de jogada.
    Foi definido um vetor para a ordem de jogadas, uma sugestão de par-ou-ímpar para a decisão da ordem pelos jogadores, depois armazenadas as varáveis char(x e o) nos índices do vetor. 
    Uma condição foi colocada em loop para restringir o jogador de escolher um char diferente dos determinados.

---
+++ jogoDaVelha/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace jogoDaVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char jogador1, jogador2 ;
+            char[] jogadores = new char[2];
+
+            Console.WriteLine("Par ou impar para decidir a ordem de jogada!");
+            
+            Console.Write("Jogador 1, escolha, X ou O: ");
+            jogador1 = Convert.ToChar(Console.ReadLine());
+
+            while(jogador1 != 'X' && jogador1 != 'O')
+            {
+                Console.Write("Jogador 1, escolha, X ou O: ");
+                jogador1 = Convert.ToChar(Console.ReadLine());
+            }
+
+            if(jogador1 == 'X')
+            {
+                jogador2 = 'O';
+
+                Console.WriteLine("Jogador 1 marca {0} e Jogador 2 marca {1}", jogador1, jogador2);
+            }
+            else
+            {
+                jogador2 = 'X';
+                Console.WriteLine("Jogador 1 marca {0} e Jogador 2 marca {1}", jogador1, jogador2);
+            }
+            
+            jogadores[0] = jogador1;
+            jogadores[1] = jogador2;
+        }
+    }
+}
